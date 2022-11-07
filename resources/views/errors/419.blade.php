6320c7884f1f73e2cff5927edffb150fd30f5742
casino-man <ryan@7-77.eu>
2022-11-07 15:07:02 -0100

    x

--- resources/views/errors/419.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('errors::minimal')
-
-@section('title', __('Session Expired'))
-@section('code', '419')
-@section('message', __('Session issues, maybe it expired?'))
--- resources/views/errors/429.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('errors::minimal')
-
-@section('title', __('Too Many Requests'))
-@section('code', '429')
-@section('message', __('Slowdown on your requests, please try again in 1 minute.'))
